edf6018aea894ccbe55a4420ea65b706753040de
LucasBorenstein1611 <lborenstein@udesa.edu.ar>
2024-11-08 12:24:48 -0300

    nada

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const formularioBusqueda = document.getElementById("formularioBusqueda");
 formularioBusqueda.addEventListener("submit", function(event) {
-    if (!validarBusqueda()) {
+    if (validarBusqueda()) {
         event.preventDefault(); // Evita el envío del formulario si es inválido
     }
 });
@@ -13,15 +13,15 @@ function validarBusqueda() {
         entradaBusqueda.placeholder = "Campo vacío";
         entradaBusqueda.style.borderColor = "red";
         console.log("Campo de texto vacío");
-        return false;
+        return true;
     } else if (terminoBusqueda.length < 3) {
         entradaBusqueda.value = ""; // Limpia la entrada para volver a intentar de cero y que se lea el mensaje
         entradaBusqueda.placeholder = "Menos de 3 caracteres";
         entradaBusqueda.style.borderColor = "red"; 
         console.log("menos de 3 caracteres");
-        return false;
+        return true;
     } else {
         entradaBusqueda.style.borderColor = ""; // Restaura el color si es válido
-        return true;
+        return false;
     }
 }
