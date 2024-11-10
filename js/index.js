a64e7cc68e352886473d6c01fd13ec6845c22160
LucasBorenstein1611 <lborenstein@udesa.edu.ar>
2024-11-10 18:31:35 -0300

    index terminado incluido js y css

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// BOTON FORMULARIO
+
 const formularioBusqueda = document.getElementById("formularioBusqueda");
 formularioBusqueda.addEventListener("submit", function(event) {
     if (validarBusqueda()) {
@@ -21,7 +23,56 @@ function validarBusqueda() {
         console.log("menos de 3 caracteres");
         return true;
     } else {
-        entradaBusqueda.style.borderColor = ""; // Restaura el color si es válido
+        entradaBusqueda.style.borderColor = ""; // Restaura el color si es válido si es que fue cambiado antes por un error
         return false;
     }
 }
+
+// CARGAR IMAGENES 
+
+// Selecciona el contenedor de recetas y el botón de "Cargar más"
+let recetasContainer = document.querySelector(".index_recetas_container");
+let botonCargar = document.querySelector(".index_boton_cargar");
+
+// Variables de control para el número de recetas mostradas
+let recetasMostradas = 0;
+const recetasPorPagina = 10;
+
+// Función para cargar recetas desde la API
+function cargarRecetas() {
+    fetch(`https://dummyjson.com/recipes?skip=${recetasMostradas}&limit=${recetasPorPagina}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            if (data.recipes) {
+                let recetasHTML = ""; 
+                for (let i = 0; i < data.recipes.length; i++) { 
+                    let receta = data.recipes[i];
+                    recetasHTML += `
+                        <article class="index_receta">
+                            <img src="${receta.image}" class="index_imagenes"/>
+                            <h2 class="index_titulo">${receta.name}</h2>            
+                            <p class="index_dificultad">Dificultad: ${receta.difficulty}</p>            
+                            <a href="detalle.html?id=${receta.id}" class="index_detalle">Ver detalle</a>
+                        </article>
+                    `;
+                }
+                recetasContainer.innerHTML += recetasHTML;
+                recetasMostradas += recetasPorPagina;
+            } else {
+                recetasContainer.innerHTML = `<p>No se encontraron recetas para mostrar.</p>`;
+            }
+        })
+        .catch(function(error) {
+            console.log(`El error es ${error}`);
+            recetasContainer.innerHTML = `<p>Error al cargar las recetas. Intenta nuevamente.</p>`;
+        });
+}
+
+// Funcion para argar más recetas cada vez que el usuario hace click en el botón
+botonCargar.addEventListener("click", cargarRecetas);
+
+// Cargar las primeras recetas al cargar la página de manera automatica
+document.addEventListener("DOMContentLoaded", cargarRecetas);
+
