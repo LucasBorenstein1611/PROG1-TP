f1b4a6078f172bff013c0227563b8c2f91fdd8af
LucasBorenstein1611 <lborenstein@udesa.edu.ar>
2024-11-15 12:52:57 -0300

    log in terminado

--- js/login.js
+++ js/login.js
@@ -1,9 +1,30 @@
-let formularioLogIn = document.querySelector("login_formulario")
-let email = document.querySelector("login_email")
-let contra = document.querySelector("login_contra")
+// Seleccionar el formulario y los campos de entrada
+let formularioLogIn = document.querySelector(".login_formulario");
+let email = document.querySelector(".login_email");
+let contra = document.querySelector(".login_contra");
 
-formularioLogIn.addEventListener("submit", function(event){
-    if (!validarBusquedaLogIn()) {
-        
+// Agregar evento al formulario
+formularioLogIn.addEventListener("submit", function (event) {
+    event.preventDefault(); // Detener el envío por defecto
+
+    let errors = false;
+
+    // Validar campo email
+    if (email.value === "") {
+        alert("Por favor complete el campo email");
+        errors = true;
     }
-})
+
+    // Validar campo contraseña
+    if (contra.value === "") {
+        alert("Por favor complete el campo contraseña");
+        errors = true;
+    }
+
+    // Si no hay errores, enviar el formulario
+    if (!errors) {
+        formularioLogIn.submit();
+    }
+});
+
+
